a0e059a48bd0b1de2f16707f0043ea34f5ed2a81
Ijebusoma <vanessaosuka@gmail.com>
2018-07-03 15:14:13 +0100

    added command line interactivity

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let apiKey = 'db11d968bb763673926197fbe0384e42';
-let city = 'London,UK';
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'db11d968bb76367****';
+let city = argv.c || 'Lagos Nigeria'; //if city is supplied, use. Else, default is Lagos
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
- const request = require('request');
+
 request(url, function(err, response, body){
     err ? console.log('error:', error): displayWeather(body);
 });
